ed145256f64e1b10ef0dc76f0510a98daac63a58
JasonMDev <jasonmansdev@gmail.com>
2015-12-21 22:02:27 +0000

    Added pdf text finder.

---
+++ wbScraper.py
@@ -0,0 +1,13 @@
+#! python3
+# wbScraper.py - DOwnload file from website and saves to file.
+# TODO: Get address from clipboard.
+
+import requests
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+res.raise_for_status()
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+
+
