abfeb04313b0e59fe8ec3ad8e9be1a14d04782c1
JasonMDev <jasonmansdev@gmail.com>
2016-08-21 21:06:06 +0100

    Completed Chapter 8.

--- CH08/randomQuizGenerator.py
+++ CH08/randomQuizGenerator.py
@@ -1,6 +1,6 @@
 #! python3
 # randomQuizGenerator.py - Creates quizzes with questions and answers in
-# random order, along with the answer key.
+# random order, along with the answer key. 
 
 import random
 
